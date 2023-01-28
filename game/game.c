26ab586f350c09e4c2134ce5ae7e15e1e21cdc98
Mark-GeorgeK <79061147+Mark-GeorgeK@users.noreply.github.com>
2023-01-29 01:00:05 +0200

    pseudorandom number generator implemented + fixing some issues

--- game/game.c
+++ game/game.c
@@ -1,37 +1,18 @@
- /******************************************************************************
- *
- * Module: application
- *
- * File Name: game.h
- *
- * Description: Source file for the snake game
- *
- *******************************************************************************/
+/******************************************************************************
+*
+* Module: application
+*
+* File Name: game.c
+*
+* Description: Source file for the snake game
+*
+*******************************************************************************/
 #include "game.h"
-#include "../drivers/uart.h"
-#include <stdlib.h>
 
-struct Point
-{
-    Point() = default;
-    explicit Point(int x_, int y_) : x{ x_ }, y{ y_ }
-    {
-
-    }
-    int x;
-    int y;
-};
-
-struct Size
-{
-    Size() = default;
-    explicit Size(int width_, int height_) : width{ width_ }, height{ height_ }
-    {
-
-    }
-    int width;
-    int height;
-};
+// Global Variables
+int frameWidth = 40;
+int frameHeight = 20;
+uint32 seed = 350; //random initialization seed
 
 /*
  * Description :
@@ -39,7 +20,7 @@ struct Size
  */
 void print_char(uint8 c)
 {
-      UART0_putchar(c);
+    UART0_putchar(c);
 }
 
 /*
@@ -48,24 +29,24 @@ void print_char(uint8 c)
  */
 uint8 get_char()
 {
-      return UART0_getchar();
+    return UART0_getchar();
 }
 
 /*
  * Description :
  * Function responsible for print a string on the terminal using UART return when encountering a newline.
  */
-void print_string(const uint8 *str)
+void print_string(const uint8* str)
 {
-	uint8 i = 0;
-	while(str[i] != '\0')
-	{
-		if (str[i] == '\n')
-		{
-                        UART0_putchar('\r');
-		}
-		UART0_putchar(str[i++]);
-	}
+    uint8 i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == '\n')
+        {
+            UART0_putchar('\r');
+        }
+        UART0_putchar(str[i++]);
+    }
 }
 
 void Game_init(void)
@@ -73,12 +54,11 @@ void Game_init(void)
     UART0_init(16000000, 9600);
 }
 
-
-// example for calling this function 
-
-// draw(std::cout, Point{6,4}, Size{10,10}, 'O', '-'}
-
-bool draw(void print_func, const Point topLeftPos, const Size rectangleSize, const char lineDrawChar = "*", const char backgroundDrawChar =" ")
+/*
+* Example for calling this function :
+* draw(std::cout, Point{6,4}, Size{10,10}, 'O', '-'}
+*/
+bool draw(void (*print_func)(const char), const Point topLeftPos, const Size rectangleSize, const char lineDrawChar, const char backgroundDrawChar)
 {
     if (topLeftPos.x < 0 || topLeftPos.y < 0 || rectangleSize.width < 1 || rectangleSize.height < 0) {
         return false;
@@ -96,10 +76,9 @@ bool draw(void print_func, const Point topLeftPos, const Size rectangleSize, con
         topLeftPos.y
     };
 
+    bool isXYOnRectangleBorder = false;
 
-    bool isXYOnRectangleBorder{ false };
-
-    for (int y = 0 ; y < rectBottomYIndex + 1; ++y)
+    for (int y = 0; y < rectBottomYIndex + 1; ++y)
     {
         for (int x = 0; x < rectRightXIndex + 1; ++x)
         {
@@ -109,7 +88,7 @@ bool draw(void print_func, const Point topLeftPos, const Size rectangleSize, con
                 {
                     isXYOnRectangleBorder = true;
                 }
-                else if ( x == rectLeftXIndex || x == rectRightXIndex)
+                else if (x == rectLeftXIndex || x == rectRightXIndex)
                 {
                     isXYOnRectangleBorder = true;
                 }
@@ -129,3 +108,34 @@ bool draw(void print_func, const Point topLeftPos, const Size rectangleSize, con
     return true;
 }
 
+/*
+ * Description :
+ * Function responsible for generating pseudorandom number
+ */
+int generatePseudorandomNumber(int max) {
+    uint32 result = seed;
+    result ^= result << 13;
+    result ^= result >> 17;
+    result ^= result << 5;
+    seed = result;
+    return result % max;
+}
+
+/*
+ * Description :
+ * Function responsible for generating pseudorandomly positioned powerup
+ */
+struct Point generatePowerup() {
+    int x = generatePseudorandomNumber(frameWidth);
+    int y = generatePseudorandomNumber(frameHeight);
+    struct Point point = { x, y };
+    return point;
+}
+
+/*
+ * Description :
+ * Function responsible for generating pseudorandomly positioned enemy
+ */
+struct Point generateEnemy() {
+    return generatePowerup();
+}
--- game/game.h
+++ game/game.h
@@ -10,9 +10,26 @@
 
 #ifndef GAME_H_
 #define GAME_H_
+#include "../drivers/uart.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#define defaultLineDrawChar '*'
+#define defaultBackgroundDrawChar ' '
+
+typedef struct Point
+{
+    int x;
+    int y;
+} Point;
+
+typedef struct Size
+{
+    int width;
+    int height;
+} Size;
 
 void Game_init(void);
-bool draw(void print_func, const Point topLeftPos, const Size rectangleSize, const char lineDrawChar = "*", const char backgroundDrawChar = " ");
+bool draw(void (*print_func)(const char), const Point topLeftPos, const Size rectangleSize, const char lineDrawChar, const char backgroundDrawChar);
 
 
 #endif /* GAME_H_ */
